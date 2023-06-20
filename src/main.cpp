359a70af0efd2c94ef3c31e4acb3859a93c6c948
Gento Aiba <130132820+aiba-gento@users.noreply.github.com>
2023-06-20 17:33:29 +0900

    2023_06_20_05:33

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,14 @@ void setup()
     MOTOR.setEnable(true);
     MOTOR.setBrake(false);
     MOTOR.setDirection(true);
-    MOTOR.setPWM(50);
+    MOTOR.setPWM(250);
+    Serial.begin(115200);
+    Serial.println("SS");
 }
 
 void loop()
 {
+    Serial.println(MOTOR.getPWM());
     MOTOR.DriveMotor();
-    delay(3);
+    delay(80);
 }
